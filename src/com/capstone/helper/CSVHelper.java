fd16f7a0cebdc0f6025c62c9816d0b9d751dec05
dandani-cs <19799004+dandani-cs@users.noreply.github.com>
2024-06-19 05:41:22 +0800

    create CSV file if not exists

--- src/com/capstone/helper/CSVHelper.java
+++ src/com/capstone/helper/CSVHelper.java
@@ -9,9 +9,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CSVHelper {
-    // TODO: check if file exists, if not create
     public static List<String> getData(String path) {
         ArrayList<String> data = new ArrayList<>();
+        validateFile(path);
         try (Scanner csvScanner = new Scanner(new File(path))) {
             while (csvScanner.hasNextLine()) {
                 data.add(csvScanner.nextLine());
@@ -23,10 +23,20 @@ public class CSVHelper {
     }
 
     public static void writeData(String path, String data) {
+        validateFile(path);
         try (FileWriter fileWriter = new FileWriter(path, true)) {
             fileWriter.append(data);
         } catch (IOException e) {
             throw new IllegalStateException("File cannot be opened", e);
+        }
+    }
+
+    private static void validateFile(String path) {
+        File file = new File(path);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new IllegalStateException("File " + path + "cannot be created" + e);
         }
     }
 }
